2fd035764d3d70b6eafa381650b7d60f41670ac9
crossxcell99 <crossxcell99@gmail.com>
2018-11-26 23:16:26 +0100

    add accounts filter and changes to open_docs_search

--- cpfa/bank_statement/doctype/bank_statement_format/bank_statement_format.js
+++ cpfa/bank_statement/doctype/bank_statement_format/bank_statement_format.js
@@ -3,19 +3,20 @@
 
 frappe.ui.form.on('Bank Statement Format', {
 	refresh: function(frm) {
-		if(frm.doc.txn_type_derivation !== 'Map From Statement'){
-			txn_types = frappe.meta.get_docfield('Bank Statement Mapping Item', 'target_field').options
-			txn_types = txn_types.split('\n')
-			txn_types.splice(txn_types.indexOf('Transaction Type'),1)
-			frm.set_df_property('target_field', 'options', txn_types, frm.doc.name, 'bank_statement_mapping_item')
-		}
+		frm.trigger('txn_type_derivation');
 	},
+	
 	txn_type_derivation: (frm)=>{
+		var txn_types = frappe.meta.get_docfield('Bank Statement Mapping Item', 'target_field').options
+		txn_types = txn_types.split('\n')
+		
 		if(frm.doc.txn_type_derivation !== 'Map From Statement'){
-			txn_types = frappe.meta.get_docfield('Bank Statement Mapping Item', 'target_field').options
-			txn_types = txn_types.split('\n')
 			txn_types.splice(txn_types.indexOf('Transaction Type'),1)
 			frm.set_df_property('target_field', 'options', txn_types, frm.doc.name, 'bank_statement_mapping_item')
+		} else {
+			frm.set_df_property('target_field', 'options', txn_types, frm.doc.name, 'bank_statement_mapping_item')
 		}
+
+		frm.refresh_field('bank_statement_mapping_item');
 	}
 });
