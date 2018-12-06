153c7442f716ab8e7387c35a4232382d9fb8157e
crossxcell99 <crossxcell99@gmail.com>
2018-12-06 21:47:31 +0100

    add voucher type query

--- cpfa/bank_statement/doctype/voucher_search_specifications/voucher_search_specifications.js
+++ cpfa/bank_statement/doctype/voucher_search_specifications/voucher_search_specifications.js
@@ -2,25 +2,62 @@
 // For license information, please see license.txt
 
 
+cur_frm.set_query('voucher_type', "voucher_search_keys", function() {
+	return{
+		query: "cpfa.utils.queries.voucher_type_query"
+	}
+});
+
 frappe.ui.form.on('Voucher Search Specifications', {
 	refresh: function(frm) {
 		for (var i = 1; i <= 10; i++){
 			let field = `field_${i}`;
 			frm.set_df_property(field, 'ignore_link_validation', true, frm.doc.name, 'voucher_search_keys')
-			frm.set_query(field, "voucher_search_keys", function() {
-				return{
-					filters: [{parent: ['=', 'Sales Invoice']}],
-					query: "cpfa.utils.queries.docfield_query"
-				}
-			});
+
+			for (var j=frm.doc.voucher_search_keys.length; j>0; j--){
+				var voucher_type = frm.doc.voucher_search_keys[j].voucher_type;
+				frm.set_query(field, "voucher_search_keys", function() {
+					return{
+						filters: [{parent: ['=', voucher_type]}],
+						query: "cpfa.utils.queries.docfield_query"
+					}
+				});
+			}
 		}
 		frm.refresh_field('voucher_search_keys');
+
 	},
-	voucher_search_keys: function(frm) {
-	}
 });
 
 frappe.ui.form.on('Voucher Search Key', 'voucher_type',
 	function(frm, dt, dn) {
+		for (var i = 1; i <= 10; i++){
+			let field = `field_${i}`;
+			frm.set_df_property(field, 'ignore_link_validation', true, frm.doc.name, 'voucher_search_keys')
+			
+			for (var j=frm.doc.voucher_search_keys.length; j>0; j--){
+				var voucher_type = locals[dt][dn]['voucher_type'];
+				frm.set_query(field, "voucher_search_keys", function() {
+					return{
+						filters: [{parent: ['=', voucher_type]}],
+						query: "cpfa.utils.queries.docfield_query"
+					}
+				});
+			}
+		}
+		frm.refresh_field('voucher_search_keys');
 	}
 );
+
+for (var i = 1; i <= 10; i++){
+	let field = `field_${i}`;
+	frappe.ui.form.on('Voucher Search Key', field, function(frm, dt, dn){
+		var doc = locals[dt][dn];
+		if (doc.search_key_specification){
+			var val = doc.search_key_specification + doc.separator||'' + doc[field]
+			frappe.model.set_value(dt, dn, "search_key_specification", val)
+		} else if(doc[field]) {
+			frappe.model.set_value(dt, dn, "search_key_specification", doc[field])
+		}
+	})
+}
--- cpfa/utils/queries.py
+++ cpfa/utils/queries.py
@@ -3,7 +3,7 @@ import frappe
 from frappe.desk.reportview import get_match_cond, get_filters_cond
 
 
- # searches for active employees
+# searches for docfield
 def docfield_query(doctype, txt, searchfield, start, page_len, filters):
 	conditions = []
 	return frappe.db.sql("""select fieldname, label, fieldtype from `tabDocField`
@@ -16,6 +16,31 @@ def docfield_query(doctype, txt, searchfield, start, page_len, filters):
 			if(locate(%(_txt)s, label), locate(%(_txt)s, label), 99999),
 			idx desc,
 			fieldname, label
+		limit %(start)s, %(page_len)s""".format(**{
+			'key': searchfield,
+			'fcond': get_filters_cond(doctype, filters, conditions),
+			'mcond': get_match_cond(doctype)
+		}), {
+			'txt': "%%%s%%" % txt,
+			'_txt': txt.replace("%", ""),
+			'start': start,
+			'page_len': page_len
+		})
+
+# searches for transaction doctypes
+def voucher_type_query(doctype, txt, searchfield, start, page_len, filters):
+	conditions = []
+	return frappe.db.sql("""select distinct df.parent, dt.module 
+		from tabDocField df join tabDocType dt on dt.name = df.parent
+		where df.fieldname in ('grand_total','net_total') and dt.istable = 0
+			and (dt.{key} like %(txt)s
+				or dt.module like %(txt)s)
+			{fcond} {mcond}
+		order by
+			if(locate(%(_txt)s, df.parent), locate(%(_txt)s, df.parent), 99999),
+			if(locate(%(_txt)s, dt.module), locate(%(_txt)s, dt.module), 99999),
+			dt.idx desc,
+			dt.parent, dt.module
 		limit %(start)s, %(page_len)s""".format(**{
 			'key': searchfield,
 			'fcond': get_filters_cond(doctype, filters, conditions),
